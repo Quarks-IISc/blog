4e3a26906701b6392d0b6105677d1fa2339c89a4
Anirudh Gupta <anirudhgupta@iisc.ac.in>
2026-03-20 14:03:19 +0530

    better css and faster loading

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -1,25 +1,5 @@
 jQuery(document).ready(function($){
 
-    //fix for stupid ie object cover
-    if (document.documentMode || /Edge/.test(navigator.userAgent)) {
-      jQuery('.featured-box-img-cover').each(function(){
-          var t = jQuery(this),
-              s = 'url(' + t.attr('src') + ')',
-              p = t.parent(),
-              d = jQuery('<div></div>');
-  
-          p.append(d);
-          d.css({
-              'height'                : '290',
-              'background-size'       : 'cover',
-              'background-repeat'     : 'no-repeat',
-              'background-position'   : '50% 20%',
-              'background-image'      : s
-          });
-          t.hide();
-      });
-    }
-
     // alertbar later
     $(document).scroll(function () {
         var y = $(this).scrollTop();
@@ -62,32 +42,30 @@ jQuery(document).ready(function($){
     });
     
     
-    // Hide Header on on scroll down
-    var didScroll;
+    // Hide Header on scroll down
     var lastScrollTop = 0;
     var delta = 5;
     var navbarHeight = $('nav').outerHeight();
+    var ticking = false;
 
     $(window).scroll(function(event){
-        didScroll = true;
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                hasScrolled();
+                ticking = false;
+            });
+            ticking = true;
+        }
     });
 
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
     function hasScrolled() {
-        var st = $(this).scrollTop();
+        var st = $(window).scrollTop();
         
         // Make sure they scroll more than delta
         if(Math.abs(lastScrollTop - st) <= delta)
             return;
 
         // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
         if (st > lastScrollTop && st > navbarHeight){
             // Scroll Down            
             $('nav').removeClass('nav-down').addClass('nav-up'); 
