613c0d39f7bf63a40439c9cbde609a17690799dc
Anirudh Gupta <anirudhgupta@iisc.ac.in>
2026-04-25 12:41:41 +0530

    improve search bar location and function

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -16,53 +16,65 @@ fetch('/search.json')
      }, this);
    });
  });
+
 function lunr_search(term) {
     if (!term || term.trim().length === 0) {
+        $('#lunrsearchresults').hide();
         return false;
     }
     if (!idx){
-       alert("Search is still loading...");
        return false;
     }
-    $('#lunrsearchresults').show();
-    $("body").addClass("modal-open")
-    setTimeout(() => {
-        document.getElementById('lunrsearch').focus();
-    }, 10);
-    if(term && term.trim().length >0) {
+
+    if (!$('#lunrsearchresults').is(':visible')) {
+        $('#lunrsearchresults').show();
+    }
+
+    if(term && term.trim().length > 0) {
         document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>";
         //put results on the screen.
         document.querySelector('#lunrsearchresults ul').innerHTML = "";
 
-        var results = idx.search(term);
+        var results = idx.search(term + "*");
         if(results.length>0){
-            //console.log(idx.search(term));
-            //if results
             for (var i = 0; i < results.length; i++) {
-                // more statements
                 var ref = results[i]['ref'];
                 var doc = documents.find(d => d.url === ref);
                
                 if (!doc) continue;
             
-                
                 var url = doc.url;
                 var title = doc.title;
                 var body = doc.content.substring(0,160)+'...';
                 document.querySelector('#lunrsearchresults ul').innerHTML += "<li class='lunrsearchresult'>" +"<a href='" + url + "'>" +"<span class='title'>" + title + "</span><br />" + "<small><span class='body'>"+ body +"</span><br />" +"<span class='url'>"+ url +"</span></small>" + "</a></li>";
-
             }
         } else {
-            document.getElementById('modtit').innerHTML =
-"<h5 class='modal-title'>Sorry, no results found. Close & try a different search!</h5>";
+            document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Sorry, no results found. Close & try a different search!</h5>";
         }
     }
     return false;
 }
 
 $(function() {
+    $('#lunrsearch').on('input', function() {
+        lunr_search(this.value);
+    });
+
     $(document).on('click' , '.btnx', function () {
         $('#lunrsearchresults').hide( 5 );
-        $( "body" ).removeClass( "modal-open" );
+    });
+
+    // Close on click outside
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.lunrsearch-wrapper').length) {
+            $('#lunrsearchresults').hide();
+        }
+    });
+
+    // Close on Escape key
+    $(document).on('keydown', function(event) {
+        if (event.key === "Escape") {
+            $('#lunrsearchresults').hide();
+        }
     });
 });
